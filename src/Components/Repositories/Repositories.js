4b88e35c1d739cfb82be16e06581ceac34914cd8
willembr <willembrants@hotmail.com>
2021-02-18 19:47:40 +0100

    creation of Repositories & Repository Components

--- src/Components/Repositories/Repositories.js
+++ src/Components/Repositories/Repositories.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './Repositories.module.scss';
+import Repository from './Repository/Repository';
 
 const Repositories = (props) => {
+
+    const repositories = props.repos.map( repository => {
+        return <Repository
+                     title = { repository.title }
+                     description = { repository.description }
+                     creationDate = { repository.creationDate }
+                     creationHour = { repository.creationHour }
+                    />
+    });
+    
     return(
         <section className={styles.Repositories}>
-
+            {repositories}
         </section>
     );
 };
--- src/Components/Repositories/Repository/Repository.js
+++ src/Components/Repositories/Repository/Repository.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './Repository.module.scss';
 
-const Repository = (props) => (
-    <article className={styles.Repository}>
+const Repository = (props) => {
 
-    </article>
-);
+    const title = <h1 className={styles.Title}>{props.title}</h1>;
+    const description = props.description ? <p>{ props.description}</p> :
+                                            <p><i>No description mentioned!</i></p>
+    const creationDate = <p>Created on {props.creationDate} {props.creationHour}</p>
+
+    return(
+        <article className={styles.Repository}>
+            {title}
+            {description}
+            <div className={styles.CreationDate}>
+                {creationDate}
+            </div>
+        </article>
+    )};
 
 export default Repository;
