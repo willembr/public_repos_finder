559c5a92be83118461bc219e57bce43ff67a4ca2
willembr <willembrants@hotmail.com>
2021-02-18 14:59:08 +0100

    Created new default stateless components Repositories and Repository with scss modules

---
+++ src/Components/Repositories/Repositories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Repositories.module.scss';
+
+const Repositories = (props) => {
+    return(
+        <section className={styles.Repositories}>
+
+        </section>
+    );
+};
+
+export default Repositories;
---
+++ src/Components/Repositories/Repository/Repository.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Repository.module.scss';
+
+const Repository = (props) => (
+    <article className={styles.Repository}>
+
+    </article>
+);
+
+export default Repository;
