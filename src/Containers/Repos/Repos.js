518635d2ca18db8070638a6d507ab2010f0f3db8
willembr <willembrants@hotmail.com>
2021-02-18 19:20:23 +0100

    Repos container = Async function to get data from Github with max render repository component

--- src/Containers/Repos/Repos.js
+++ src/Containers/Repos/Repos.js
@@ -4,26 +4,37 @@ import { setDate, setHour, setCapitalLetter } from '../../Functions/Output';
 import Spinner from '../../Components/UI/Spinner/Spinner';
 import Repositories from '../../Components/Repositories/Repositories';
 
+
 class Repos extends Component{
+
     state = {
         repos : [],
         user: '',
-        loading:false
+        loading:false,
+        error:false
     }
 
-     componentDidMount(){ this.getReposHandler(this.props.match.params.user);}
-     componentDidUpdate(){ if(this.newSearch()) this.getReposHandler(this.props.match.params.user);}
+    componentDidMount(){ this.getRepos(this.props.match.params.user) }
+    componentDidUpdate(){ if(this.newSearch()) this.getRepos(this.props.match.params.user) }
 
-     newSearch(){ return this.state.user !== this.props.match.params.user;}
-     
-     getReposHandler = (user) => {
-            const url = `users/${user}/repos`
-            Axios.get(url)
-                 .then( response => {
-                    this.setState({ loading : true })
-                    const updatedRepositories = response.data.map( repository => {
-                        const created = repository.created_at;
-                        return {
+    newSearch(){ 
+        if(this.state.user === '') return false;
+        return this.state.user !== this.props.match.params.user;
+        }
+
+    getRepos = async(user) => {
+        const url = `users/${user}/repos`;
+        let response = '';
+        
+            try{
+                response = await Axios.get(url);
+            } catch (error) {
+                this.setState({error:true})
+            }
+            const updatedRepositories = await response.data.map( 
+                repository => {
+                    const created = repository.created_at;
+                    return {
                             title: setCapitalLetter(repository.name),
                             description: setCapitalLetter(repository.description),
                             stars:repository.stargazers_count,
@@ -31,29 +42,27 @@ class Repos extends Component{
                             forks:repository.forks_count,
                             creationDate: setDate(created),
                             creationHour: setHour(created)
-                        }
-                    });
-                     this.setState({
-                         repos:updatedRepositories,
-                         user: user,
-                         loading:false
-                     });
-                 })
-                 .catch( error => {
-                     console.log(error);
-                 })
-     }
+                           }
+                             });
+              this.setState({
+                    repos:updatedRepositories,
+                    loading:false,
+                    user:user,
+                    error:false
+             })
+
+   
+    }
 
     render(){
-         let content = "";
-         if( this.newSearch() )
-         {
-            this.state.loading ? content = <Spinner/> : content = <Repositories repos={this.state.repos}/>
-         }
+        let content,errorMessage = "";
+        if(this.state.repos.length > 0 && (this.state.user === this.props.match.params.user)) content = <Repositories repos = {this.state.repos}/>
+        if(this.state.error || this.state.repos.length === 0 ) errorMessage = <p>We couldn't find any Repos</p>;
         return(
-            <>
+            <p>
             {content}
-            </>
+            {errorMessage}
+            </p>
         );
     }
 };
