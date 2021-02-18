188ea7c107bc69a65df23409acb998b9aea19a40
willembr <willembrants@hotmail.com>
2021-02-18 15:20:08 +0100

    new loading state + change on getreposhandler to show spinner when data is loading

--- src/Containers/Repos/Repos.js
+++ src/Containers/Repos/Repos.js
@@ -1,20 +1,26 @@
 import React,{Component} from 'react';
 import Axios from '../../hoc/Axios';
 import { setDate, setHour, setCapitalLetter } from '../../Functions/Output';
+import Spinner from '../../Components/UI/Spinner/Spinner';
+import Repositories from '../../Components/Repositories/Repositories';
 
 class Repos extends Component{
     state = {
         repos : [],
-        user: ''
+        user: '',
+        loading:false
     }
 
      componentDidMount(){ this.getReposHandler(this.props.match.params.user);}
      componentDidUpdate(){ if(this.newSearch()) this.getReposHandler(this.props.match.params.user);}
+
+     newSearch(){ return this.state.user !== this.props.match.params.user;}
      
      getReposHandler = (user) => {
             const url = `users/${user}/repos`
             Axios.get(url)
                  .then( response => {
+                    this.setState({ loading : true })
                     const updatedRepositories = response.data.map( repository => {
                         const created = repository.created_at;
                         return {
@@ -29,26 +35,25 @@ class Repos extends Component{
                     });
                      this.setState({
                          repos:updatedRepositories,
-                         user: user
+                         user: user,
+                         loading:false
                      });
                  })
                  .catch( error => {
                      console.log(error);
                  })
-        }
-      newSearch(){ 
-           return this.state.user !== this.props.match.params.user;
-        }
+     }
 
     render(){
+         let content = "";
          if( this.newSearch() )
          {
-            console.log('new post');
+            this.state.loading ? content = <Spinner/> : content = <Repositories repos={this.state.repos}/>
          }
         return(
-            <div>
-
-            </div>
+            <>
+            {content}
+            </>
         );
     }
 };
