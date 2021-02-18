7d6cab063b70bc129c418ab36f5526cc3670191e
willembr <willembrants@hotmail.com>
2021-02-18 20:57:09 +0100

    changed the css to align all repos on responsive webdesign

--- src/Components/Repositories/Repository/Repository.js
+++ src/Components/Repositories/Repository/Repository.js
@@ -4,8 +4,8 @@ import styles from './Repository.module.scss';
 const Repository = (props) => {
 
     const title = <h1 className={styles.Title}>{props.title}</h1>;
-    const description = props.description ? <p>{ props.description}</p> :
-                                            <p><i>No description mentioned!</i></p>
+    const description = props.description ? <p className={styles.Description}>{ props.description}</p> :
+                                            <p className={styles.Description}><i>No description mentioned!</i></p>
     const creationDate = <p>Created on {props.creationDate} {props.creationHour}</p>
 
     return(
