15ca81bb194946ddbef15fb6590e82126e80ba54
willembr <willembrants@hotmail.com>
2021-02-18 11:32:08 +0100

    Test connection with github to upload repos from user inside Repos container

--- src/Containers/Repos/Repos.js
+++ src/Containers/Repos/Repos.js
@@ -1,6 +1,24 @@
 import React,{Component} from 'react';
+import Axios from '../../hoc/Axios';
 
 class Repos extends Component{
+    state = {
+        repos : []
+    }
+
+    componentDidMount(){
+        if(this.props.match.params.user){
+            const url = `users/${this.props.match.params.user}/repos`
+            Axios.get(url)
+                 .then( response => {
+                     console.log(response.data);
+                 })
+                 .catch( error => {
+                     console.log(error);
+                 })
+        }
+    }
+
     render(){
         return(
             <div>
