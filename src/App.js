3a3225f8f1465aa30f41a1de812c132dbcd7c95f
willembr <willembrants@hotmail.com>
2021-02-18 10:26:05 +0100

    Insert BrowserRouter inside App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import styles from './App.module.scss';
 import User from './Containers/User/User';
+import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className={styles.App}>
+      <BrowserRouter>
         <h1 className={styles.Title}>Public Repository Finder</h1>
         <User/>
+      </BrowserRouter>  
     </div>
   );
 }
