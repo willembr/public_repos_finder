0d8ff63ebb90d57720350817a1fb3e5beeae1322
willembr <willembrants@hotmail.com>
2021-02-18 11:30:51 +0100

    inserted Route compent inside Main with path="/:user" and component = {Repos}

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -2,13 +2,15 @@ import React,{Component} from 'react';
 import User from '../User/User';
 import styles from './Main.module.scss';
 import { Route } from 'react-router-dom';
+import Repos from '../Repos/Repos';
 
 class Main extends Component{
     render(){
         return(
             <section className={styles.Main}>
                 <h1 className={styles.Title}>Public Repository Finder</h1>
-                <Route path="/" component={User}/>
+                <Route path="/" component={ User } />
+                <Route path="/:user" component={ Repos } />
             </section>
         );
     }
