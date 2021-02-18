197cd15aa992cc26b97ceea222fe2eca705b36ef
willembr <willembrants@hotmail.com>
2021-02-18 15:17:59 +0100

    created a Spinner component inside UI to show when loading data

---
+++ src/Components/UI/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Spinner.module.scss';
+
+const Spinner = () => (
+    <div className={classes.Spinner}>
+        Loading...
+    </div>
+);
+
+export default Spinner;
