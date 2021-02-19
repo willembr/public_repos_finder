13da4b4ed564414dd1c3ed9c3c3a0c6cac6269e8
willembr <willembrants@hotmail.com>
2021-02-19 11:30:06 +0100

    inserted token voor GitHub Api

--- src/hoc/Axios.js
+++ src/hoc/Axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const githubToken = '88dee40beb64edc2848a496f3331cce6e72ec6c6';
+
 const instance = axios.create({
-    baseURL:"https://api.github.com/"
+    baseURL:"https://api.github.com/",
+    headers: {"Authorization" : "token " + githubToken}
 });
 
 export default instance;
