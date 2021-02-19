53c24ce70583023a53d036f2f93cf1492b39af06
willembr <willembrants@hotmail.com>
2021-02-19 11:59:32 +0100

    provided errormessage or no content for repositories inside Repositories component

--- src/Components/Repositories/Repositories.js
+++ src/Components/Repositories/Repositories.js
@@ -3,19 +3,24 @@ import styles from './Repositories.module.scss';
 import Repository from './Repository/Repository';
 
 const Repositories = (props) => {
+    let repositories, errorMessage = "";
 
-    const repositories = props.repos.map( repository => {
-        return <Repository
-                     title = { repository.title }
-                     description = { repository.description }
-                     creationDate = { repository.creationDate }
-                     creationHour = { repository.creationHour }
-                    />
-    });
-    
+    if(!props.noReps && !props.noUser)
+        repositories = props.repos.map( repository => {
+                            return <Repository
+                                        title = { repository.title }
+                                        description = { repository.description }
+                                        creationDate = { repository.creationDate }
+                                        creationHour = { repository.creationHour }
+                                    />
+                                    });
+
+    if(props.noRepos) errorMessage = <p className={styles.ErrorMessage}>We were unable to find any repositories for this User</p>;
+
     return(
         <section className={styles.Repositories}>
             {repositories}
+            {errorMessage}
         </section>
     );
 };
