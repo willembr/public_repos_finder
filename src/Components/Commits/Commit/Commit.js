73f83d09e88ca64353a729b5d2ce1520000506f1
willembr <willembrants@hotmail.com>
2021-02-20 20:46:16 +0100

    Set default setup for commits and commit component

---
+++ src/Components/Commits/Commit/Commit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Commit.modules.scss';
+
+const Commit = (props) => {
+    return(
+        <div className={styles.Commit}>
+
+        </div>
+    );
+};
+
+export default Commit;
---
+++ src/Components/Commits/Commits.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Commits.modules.scss';
+
+const Commits = (props) => {
+    console.log(props.commits);
+    return(
+        <section className={styles.Commit}>
+
+        </section>
+    );
+};
+
+export default Commits;
