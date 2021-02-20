b3a17c08e64e9b42680658f06ffef1e3372d57a2
willembr <willembrants@hotmail.com>
2021-02-20 07:40:37 +0100

    Added reducer User inside store/reducers

---
+++ src/Store/Reducers/User.js
@@ -0,0 +1,11 @@
+import * as actionTypes from '../Actions/ActionTypes';
+
+const initialState = {
+    user:null
+}
+
+const reducer = ( state = initialState, action ) => {
+    return state;
+}
+
+export default reducer;
