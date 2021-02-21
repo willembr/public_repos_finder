cf05e942c0aec517c2a6fc4bf654f6ee707222c6
willembr <willembrants@hotmail.com>
2021-02-21 08:07:28 +0100

    New component commit_item

---
+++ src/Components/Commits/Commit/commit_item/commit_item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './commit.module.scss';
+
+const commit_item = (props) => {
+    return(
+        <div className={styles.Commit_Item}>
+
+        </div>
+    );
+};
+
+export default commit_item;
